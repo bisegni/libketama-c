eb67fe924b9273d8ff100682516731754a706895
Claudio Bisegni <Claudio.Bisegni@lnf.infn.it>
2015-08-19 13:33:14 +0200

    Initial Commit

---
+++ libketama/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libketama
+//
+//  Created by Claudio Bisegni on 19/08/15.
+//  Copyright (c) 2015 Claudio Bisegni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
